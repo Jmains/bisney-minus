ebb1dc9500017ab2bf3a4be621a5292dd4538df6
Jackson <jacksonmain9@gmail.com>
2021-07-03 13:08:16 -0500

    improved css semantics and created acc menu component

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,160 +1,85 @@
 import s from "./Navbar.module.css";
 import { Film, Home, Plus, Search, Star, Camera } from "../icons/index";
 import { useState } from "react";
-
-const profiles = [
-  {
-    name: "jack",
-    picUrl:
-      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
-  },
-  {
-    name: "jill",
-    picUrl:
-      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
-  },
-];
-
-const options = [
-  {
-    name: "Edit Profiles",
-    to: "#",
-  },
-  {
-    name: "App Settings",
-    to: "#",
-  },
-  {
-    name: "Account",
-    to: "#",
-  },
-  {
-    name: "Help",
-    to: "#",
-  },
-  {
-    name: "Log Out",
-    to: "#",
-  },
-];
+import AccountMenu from "./accountMenu/AccountMenu";
 
 export default function navbar() {
   const [showAccountMenu, setShowAccountMenu] = useState(false);
 
-  const AccountMenu = () => {
-    return (
-      <div
-        onMouseLeave={async () => {
-          await setShowAccountMenu(false);
-        }}
-        className={s.accMenuContainer}
-      >
-        <div className={s.accNameContainer}>
-          <p className={s.accName}>Aneem</p>
-          <img
-            className={s.profileImg}
-            src="https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-          />
-        </div>
-
-        <div className={s.separator}></div>
-        {profiles.map((profile, i) => {
-          return (
-            <div key={i} className={s.userAccsContainer}>
-              <img className={s.profileImg} src={profile.picUrl} />
-              <p className={s.profileName}>{profile.name}</p>
-            </div>
-          );
-        })}
-
-        {options.map((opt, i) => {
-          return (
-            <div key={i} className={s.menuItemContainer}>
-              <a className={s.menuItem} href={opt.to}>
-                {opt.name}
-              </a>
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
-
   return (
-    <div className={s.navContainer}>
-      <div>
-        <div className={s.navListContainer}>
-          <div className={s.navItemContainer}>
-            <a className={s.navLogo} href="#">
-              Logo
-            </a>
-          </div>
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="HOME" className={s.navItemContainer} href="#">
-              <span className={s.navIconContainer}>
-                <Home className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Home</p>
-            </a>
-          </span>
-
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="SEARCH" className={s.navItemContainer} href="#">
-              <span className={s.navIconContainer}>
-                <Search className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Search</p>
-            </a>
-          </span>
-
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="WATCHLIST" className={s.navItemContainer} href="#">
-              <span className={s.navIconContainer}>
-                <Plus className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Watchlist</p>
-            </a>
-          </span>
-
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="ORIGINALS" className={s.navItemContainer} href="#">
-              <span>
-                <Star className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Originals</p>
-            </a>
-          </span>
-
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="FILM" className={s.navItemContainer} href="#">
-              <span>
-                <Film className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Movies</p>
-            </a>
-          </span>
-
-          <span className={s.navItemWrapper}>
-            <a tabIndex="0" aria-label="SERIES" className={s.navItemContainer} href="#">
-              <span>
-                <Camera className={s.navIcon} />
-              </span>
-              <p className={s.navItem}>Series</p>
-            </a>
-          </span>
+    <div className={s.nav__container}>
+      <div className={s.nav__listContainer}>
+        <div className={s.nav__link}>
+          <a className={s.nav__logo} href="#">
+            Bisney
+          </a>
         </div>
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="HOME" className={s.nav__link} href="#">
+            <span className={s.nav__iconContainer}>
+              <Home className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Home</p>
+          </a>
+        </span>
+
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="SEARCH" className={s.nav__link} href="#">
+            <span className={s.nav__iconContainer}>
+              <Search className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Search</p>
+          </a>
+        </span>
+
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="WATCHLIST" className={s.nav__link} href="#">
+            <span className={s.nav__iconContainer}>
+              <Plus className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Watchlist</p>
+          </a>
+        </span>
+
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="ORIGINALS" className={s.nav__link} href="#">
+            <span>
+              <Star className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Originals</p>
+          </a>
+        </span>
+
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="FILM" className={s.nav__link} href="#">
+            <span>
+              <Film className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Movies</p>
+          </a>
+        </span>
+
+        <span className={s.nav__linkContainer}>
+          <a tabIndex="0" aria-label="SERIES" className={s.nav__link} href="#">
+            <span>
+              <Camera className={s.nav__icon} />
+            </span>
+            <p className={s.nav__linkText}>Series</p>
+          </a>
+        </span>
       </div>
+
       <div
         onMouseEnter={async () => {
           await setShowAccountMenu(true);
         }}
-        className={s.profileImgContainer}
+        className={s.nav__profileImgContainer}
       >
         {showAccountMenu ? (
-          <AccountMenu />
+          <AccountMenu setShowAccountMenu={setShowAccountMenu} />
         ) : (
           <img
-            className={s.profileImg}
+            className={s.nav__profileImg}
             src="https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
           />
         )}
---
+++ components/navbar/accountMenu/AccountMenu.jsx
@@ -0,0 +1,76 @@
+import s from "./AccountMenu.module.css";
+
+export default function AccountMenu({ setShowAccountMenu }) {
+  return (
+    <div
+      onMouseLeave={async () => {
+        await setShowAccountMenu(false);
+      }}
+      className={s.accMenu__container}
+    >
+      <div className={s.accMenu__profile}>
+        <p className={s.accMenu__profileName}>Aneem</p>
+        <img
+          className={s.accMenu__profileImg}
+          src="https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
+        />
+      </div>
+
+      <div className={s.accMenu__separator}></div>
+      {profiles.map((profile, i) => {
+        return (
+          <div key={i} className={s.accMenu__userProfilesContainer}>
+            <img className={s.accMenu__userProfilesImg} src={profile.picUrl} />
+            <p className={s.accMenu__userProfilesName}>{profile.name}</p>
+          </div>
+        );
+      })}
+
+      {options.map((opt, i) => {
+        return (
+          <div key={i} className={s.accMenu__linkContainer}>
+            <a className={s.accMenu__link} href={opt.to}>
+              {opt.name}
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const profiles = [
+  {
+    name: "jack",
+    picUrl:
+      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+  },
+  {
+    name: "jill",
+    picUrl:
+      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80",
+  },
+];
+
+const options = [
+  {
+    name: "Edit Profiles",
+    to: "#",
+  },
+  {
+    name: "App Settings",
+    to: "#",
+  },
+  {
+    name: "Account",
+    to: "#",
+  },
+  {
+    name: "Help",
+    to: "#",
+  },
+  {
+    name: "Log Out",
+    to: "#",
+  },
+];
