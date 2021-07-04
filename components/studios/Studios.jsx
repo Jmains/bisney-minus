2a730eaa7c0ea2cf0aae2198eb9205910e913e9a
Jackson <jacksonmain9@gmail.com>
2021-07-04 15:46:55 -0500

    added scale on hover

--- components/studios/Studios.jsx
+++ components/studios/Studios.jsx
@@ -3,11 +3,21 @@ import s from "./Studios.module.css";
 export default function Studios() {
   return (
     <div className={s.studios__container}>
-      <div className={s.studios__link}>{/* Bisney */}</div>
-      <div className={s.studios__link}>{/* Bixar */}</div>
-      <div className={s.studios__link}>{/* Barvel */}</div>
-      <div className={s.studios__link}>{/* Bar Wars */}</div>
-      <div className={s.studios__link}>{/* Bashional Beographic */}</div>
+      <div role="link" className={s.studios__link}>
+        {/* Bisney */}
+      </div>
+      <div role="link" className={s.studios__link}>
+        {/* Bixar */}
+      </div>
+      <div role="link" className={s.studios__link}>
+        {/* Barvel */}
+      </div>
+      <div role="link" className={s.studios__link}>
+        {/* Bar Wars */}
+      </div>
+      <div role="link" className={s.studios__link}>
+        {/* Bashional Beographic */}
+      </div>
     </div>
   );
 }
