858752ad9a64480dd9e9a33ca072f9f7328c8e84
Jackson <jacksonmain9@gmail.com>
2021-07-18 20:33:17 -0500

    added movies section

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -2,13 +2,13 @@ import s from "./Card.module.css";
 
 const Card = ({ img }) => {
   return (
-    <div>
-      <a className={`${s.card}`} href="#">
-        <div className={s.card__imgContainer}>
+    <a className={s.card} href="#">
+      <div className={s.card__imgContainer}>
+        <div>
           <img className={s.card__img} src={img.url} alt="" />
         </div>
-      </a>
-    </div>
+      </div>
+    </a>
   );
 };
 
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "../navbar/Navbar";
 import s from "./Layout.module.css";
 export default function Layout({ children }) {
   return (
-    <div style={{ position: "relative" }}>
+    <div>
       <Navbar />
       <div className={s.bg__color}></div>
       <main className={s.layout}>{children}</main>
